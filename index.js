d252c585d8bcf11a916bbc9d980935640425888e
Maksym Skrypnyk <maksym.skrypnyk@customertimes.com>
2021-10-07 13:09:57 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const qr = require('qrcode');
+
+const app = express();
+const PORT = 5000;
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended : false
+}));
+app.use(bodyParser.json());
+
+app.get("/generate", (req, res) => {
+    const url = req.query.data;
+
+    if(!url) {
+        res.status(400);
+    }
+
+    qr.toDataURL(url, (err, src) => {
+        if(err) {
+            res.status(400);
+        }
+
+        res.status(200).send(src);
+    })
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
